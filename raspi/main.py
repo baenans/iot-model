2969f4f0b3dde779bd048c43d96f0d4d4e42b5eb
Francisco Baena <baena.francisco@gmail.com>
2018-11-30 16:31:34 +0000

    raspi: updates dashboard information about temperature depending on values of device

--- raspi/main.py
+++ raspi/main.py
@@ -1,4 +1,5 @@
 import json, time
+import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import sensorevent_pb2
 from google.cloud import pubsub_v1
@@ -6,13 +7,31 @@ from google.cloud import pubsub_v1
 PUBLISHER = pubsub_v1.PublisherClient()
 TOPIC_NAME = PUBLISHER.topic_path('temp-humidity-monitoring', 'device-ingest')
 
+BUZZER = 26
+TEMP_LED_RED = 5
+TEMP_LED_YELLOW = 6
+TEMP_LED_GREEN = 13
+
 def on_connect(client, userdata, flags, rc):
   client.subscribe("events", 2)
 
 def send_to_the_clouds(data):
   json_data = json.dumps(data)
-  PUBLISHER.publish(TOPIC_NAME, json_data)
+  # PUBLISHER.publish(TOPIC_NAME, json_data)
   print json_data
+
+def _updateTempLeds(green, yellow, red):
+  GPIO.output(TEMP_LED_GREEN, GPIO.HIGH if green else GPIO.LOW)
+  GPIO.output(TEMP_LED_YELLOW, GPIO.HIGH if yellow else GPIO.LOW)
+  GPIO.output(TEMP_LED_RED, GPIO.HIGH if red else GPIO.LOW)
+
+def updateTemperature(temp):
+  if  temp < 20:
+    _updateTempLeds(True, False, False)
+  elif temp >= 20 and temp < 30:
+    _updateTempLeds(False, True, False)
+  else:
+    _updateTempLeds(False, False, True)
 
 def on_message(client, userdata, msg):
   event = sensorevent_pb2.SensorEvent()
@@ -29,6 +48,11 @@ def main():
   client.on_connect = on_connect
   client.on_message = on_message
   client.connect("localhost", 1883)
+
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(TEMP_LED_RED, GPIO.OUT)
+  GPIO.setup(TEMP_LED_YELLOW, GPIO.OUT)
+  GPIO.setup(TEMP_LED_GREEN, GPIO.OUT)
 
   client.loop_forever()
 
