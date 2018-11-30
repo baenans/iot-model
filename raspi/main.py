617f42a864c99b090e54cd1fa32a18a5a95b4d56
Francisco Baena <baena.francisco@gmail.com>
2018-11-30 15:00:38 +0000

    changes indent to 2 spaces

--- raspi/main.py
+++ raspi/main.py
@@ -7,29 +7,30 @@ PUBLISHER = pubsub_v1.PublisherClient()
 TOPIC_NAME = PUBLISHER.topic_path('temp-humidity-monitoring', 'device-ingest')
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe("events", 2)
+  client.subscribe("events", 2)
 
 def send_to_the_clouds(data):
-    json_data = json.dumps(data)
-    PUBLISHER.publish(TOPIC_NAME, json_data)
-    print json_data
+  json_data = json.dumps(data)
+  PUBLISHER.publish(TOPIC_NAME, json_data)
+  print json_data
 
 def on_message(client, userdata, msg):
-    event = sensorevent_pb2.SensorEvent()
-    event.ParseFromString(msg.payload)
-    send_to_the_clouds({
-        'timestamp': event.timestamp,
-        'temperature': event.temperature,
-        'humidity': event.humidity,
-        'device_id': event.deviceId
-    })
+  event = sensorevent_pb2.SensorEvent()
+  event.ParseFromString(msg.payload)
+  send_to_the_clouds({
+    'timestamp': event.timestamp,
+    'temperature': event.temperature,
+    'humidity': event.humidity,
+    'device_id': event.deviceId
+  })
 
 def main():
-    client = mqtt.Client("hub-device")
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect("localhost", 1883)
+  client = mqtt.Client("hub-device")
+  client.on_connect = on_connect
+  client.on_message = on_message
+  client.connect("localhost", 1883)
 
-    client.loop_forever()
+  client.loop_forever()
 
-main()
+if __name__ == '__main__':
+  main()
