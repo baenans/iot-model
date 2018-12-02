1badc372ff152f7c462ec56a52f3f29a00fd2c86
Francisco Baena <baena.francisco@gmail.com>
2018-12-02 15:00:33 +0000

    topic subscription

--- raspi/main.py
+++ raspi/main.py
@@ -28,6 +28,7 @@ class HubDevice:
     self.MQTT_CLIENT.on_message = self.on_message
     self.MQTT_CLIENT.connect("localhost", 1883)
     self.configureGPIO()
+    self.subscribe_to_rgb_actuator()
     self.MQTT_CLIENT.loop_forever()
 
   # Callback of MQTT connect, subscribes to ESP8266 Temp Sensor
